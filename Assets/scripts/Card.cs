b9db9d53325e6a33bc6a118d4a3fff5454482b44
alice <alice.gabbana@gmail.com>
2018-02-04 12:39:03 +0100

    Initial commit

---
+++ Assets/scripts/Card.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour {
+
+    public static bool DO_NOT = false;
+
+    [SerializeField]
+    private int _state;
+    [SerializeField]
+    private int _cardValue;
+    [SerializeField]
+    private bool _initialized = false;
+
+    private Sprite _cardBack;
+    private Sprite _cardFace;
+
+    private GameObject _mananager;
+
+    private void Start()
+    {
+        _mananager = GameObject.FindGameObjectWithTag("Manager");
+        _state = 1;
+    }
+
+    public void setupGraphics()
+    {
+        _cardBack = _mananager.GetComponent<GameManager>().getCardBack();
+        _cardFace = _mananager.GetComponent<GameManager>().getCardFace(_cardValue);
+        flipCard();
+    }
+
+    public void flipCard()
+    {
+        if (_state == 1 && !DO_NOT) _state = 0;
+        else if (_state == 0 && !DO_NOT) _state = 1;
+        if (_state == 0 && !DO_NOT)
+        {
+            GetComponent<Image>().sprite = _cardBack;
+        }
+        else if (_state == 1 && !DO_NOT)
+        {
+            GetComponent<Image>().sprite = _cardFace;
+        }
+    }
+
+    public int cardValue
+    {
+        get { return _cardValue; }
+        set { _cardValue = value; }
+    }
+
+    public int state
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    public bool initialized
+    {
+        get { return _initialized; }
+        set { _initialized = value; }
+    }
+
+    public void falseCheck(int x)
+    {
+        StartCoroutine(pause(x));
+    }
+
+    IEnumerator pause(int x)
+    {
+        Debug.Log(string.Format("Matche = {0}", x));
+        if ( x == 2) yield return new WaitForSeconds(0);
+        else yield return new WaitForSeconds(1);
+        if (_state == 0)
+        {
+            GetComponent<Image>().sprite = _cardBack;
+        } else if (_state == 1)
+        {
+            GetComponent<Image>().sprite = _cardFace;
+        }
+        DO_NOT = false;
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Project Title
+
+First project using Unity : Memory game !
+
+## Getting Started
+
+It's unity project just open it with Unity (i made it with version 2017.3.0f3)
+
+### Installing
+
+The project has 2 scenes : first scene is Menu and the game scene is level
+
+## History
+
+* I followed this tutorial : https://www.youtube.com/watch?v=fCGdaQ50DbA
+* Then I've fixed two things - fix bug of card change state by mistake / delay before to play again not necessary if a pair is found : this is my first commit
+* I need to add several improvements and features this is the next step : add a timer / add raimbow & sparkles in case of victory / add malus card : pick it up and all the card are shuffle or add bonus card : 3 same card to find but only one time etc ...
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
